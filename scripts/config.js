e5f46fd940d28ed60704f62fa03e55ac5dc4421b
talmzip <89967688+talmzip@users.noreply.github.com>
2025-11-11 18:05:40 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  submitUrl: "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL",
+  submitUrl: "https://script.google.com/macros/s/AKfycbxMrTAfQDzJNNMIvnOzSP6ePmxuZAto5Ea5xqJCOHuacAvFZFHkqYn144B4O4fx1rEh/exec",
   transitionDuration: 400,
   transitionEase: "cubic-bezier(0.4, 0, 0.2, 1)",
 };
